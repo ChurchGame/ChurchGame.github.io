3dcb88e79ce8f03be2d052db589612ba3fa84b40
Lorenzo Fiaschi <35898216+LoreFiaschi@users.noreply.github.com>
2024-06-30 17:06:59 +0200

    test

---
+++ hard_reload.js
@@ -0,0 +1,14 @@
+async function handleHardReload(url) {
+    await fetch(url, {
+        headers: {
+            Pragma: 'no-cache',
+            Expires: '-1',
+            'Cache-Control': 'no-cache',
+        },
+    });
+    window.location.href = url;
+    // This is to ensure reload with url's having '#'
+    window.location.reload();
+}
+
+setTimeout(handleHardReload(window.location.href), 30000);
